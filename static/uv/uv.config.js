dc81899d1920b2be5539189941d3a1eb58da52db
justin <58222040+splashun@users.noreply.github.com>
2025-05-06 14:26:31 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://b.chiefkeef.uk/bare',
+    bare: "https://science.petezahgames.com/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
